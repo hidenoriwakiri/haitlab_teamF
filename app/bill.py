a3c2826377322d5096811a53208144241b2451c4
岩切　秀規 <rtuocck2.020.hip@gmail.com>
2020-03-28 23:18:54 +0900

    app最新版

--- app/bill.py
+++ app/bill.py
@@ -25,7 +25,7 @@ name = {
 'satumaimo':'さつまいも天'
 }
 
-dnDir = '/Users/hidenori/Desktop/darknet/'; #darknetディレクトリへのパス(pythonの場合, ホームディレクトリを"~"とするとうまくいかないようです, txt_nameと繋げるため, 必ず最後に"/"をつけてください)
+dnDir = '/パスを書いてください/'; #darknetディレクトリへのパス(pythonの場合, ホームディレクトリを"~"とするとうまくいかないようです, txt_nameと繋げるため, 必ず最後に"/"をつけてください)
 txt_name = 'd.txt'
 txt_path = dnDir+txt_name
 
